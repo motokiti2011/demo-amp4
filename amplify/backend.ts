f885dd70ffc20c89b8484f493c3f7123c873e6ae
motokiti2011 <motokiti2011@gmail.com>
2024-07-16 21:57:00 +0900

    716 work

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,11 +1,21 @@
 import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
 import { data } from './data/resource';
+import { helloWorld } from './function/hello-world/resource';
 
 /**
  * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
  */
-defineBackend({
+const backend = defineBackend({
   auth,
   data,
+  helloWorld, // Lambda
+});
+const authenticatedUserIamRole = backend.auth.resources.authenticatedUserIamRole;
+backend.helloWorld.resources.lambda.grantInvoke(authenticatedUserIamRole);
+
+backend.addOutput({
+  custom: {
+    helloWorldFunctionName: backend.helloWorld.resources.lambda.functionName,
+  },
 });
