69bacf3b576e19a68982bff8c4f866e2abc1f539
motokiti2011 <motokiti2011@gmail.com>
2024-07-21 09:13:21 +0900

    721 test2

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -6,41 +6,89 @@ adding a new "isDone" field as a boolean. The authorization rule below
 specifies that any unauthenticated user can "create", "read", "update", 
 and "delete" any "Todo" records.
 =========================================================================*/
-const schema = a.schema({
-  Todo: a
-    .model({
-      content: a.string(),
-    })
-    .authorization((allow) => [allow.guest()]),
-  Customer: a
-  .model({
-    customerId: a.id().required(),
-    // fields can be of various scalar types,
-    // such as string, boolean, float, integers etc.
-    name: a.string(),
-    // fields can be of custom types
-    location: a.customType({
-      // fields can be required or optional
-      lat: a.float().required(),
-      long: a.float().required(),
-    }),
-      // fields can be enums
-      engagementStage: a.enum(["PROSPECT", "INTERESTED", "PURCHASED"]),
-      collectionId: a.id(),
-      collection: a.belongsTo("Collection", "collectionId")
-      // Use custom identifiers. By default, it uses an `id: a.id()` field
-    })
-    .identifier(["customerId"]),
-  });
+// const schema = a.schema({
+//   Todo: a
+//     .model({
+//       content: a.string(),
+//     })
+//     .authorization((allow) => [allow.guest()]),
+//   Customer: a
+//   .model({
+//     customerId: a.id().required(),
+//     // fields can be of various scalar types,
+//     // such as string, boolean, float, integers etc.
+//     name: a.string(),
+//     // fields can be of custom types
+//     location: a.customType({
+//       // fields can be required or optional
+//       lat: a.float().required(),
+//       long: a.float().required(),
+//     }),
+//       // fields can be enums
+//       engagementStage: a.enum(["PROSPECT", "INTERESTED", "PURCHASED"]),
+//       collectionId: a.id(),
+//       collection: a.belongsTo("Collection", "collectionId")
+//       // Use custom identifiers. By default, it uses an `id: a.id()` field
+//     })
+//     .identifier(["customerId"]),
+//   });
+
+// export type Schema = ClientSchema<typeof schema>;
+
+// export const data = defineData({
+//   schema,
+//   authorizationModes: {
+//     defaultAuthorizationMode: 'iam',
+//   },
+// });
+
+
+// import { type ClientSchema, a, defineData } from '@aws-amplify/backend';
+
+const schema = a
+  .schema({
+    Customer: a
+      .model({
+        customerId: a.id().required(),
+        // fields can be of various scalar types,
+        // such as string, boolean, float, integers etc.
+        name: a.string(),
+        // fields can be of custom types
+        location: a.customType({
+          // fields can be required or optional
+          lat: a.float().required(),
+          long: a.float().required(),
+        }),
+        // fields can be enums
+        engagementStage: a.enum(["PROSPECT", "INTERESTED", "PURCHASED"]),
+        collectionId: a.id(),
+        collection: a.belongsTo("Collection", "collectionId")
+        // Use custom identifiers. By default, it uses an `id: a.id()` field
+      })
+      .identifier(["customerId"]),
+    Collection: a
+      .model({
+        customers: a.hasMany("Customer", "collectionId"), // setup relationships between types
+        tags: a.string().array(), // fields can be arrays
+        representativeId: a.id().required(),
+        // customize secondary indexes to optimize your query performance
+      })
+      .secondaryIndexes((index) => [index("representativeId")]),
+  })
+  .authorization((allow) => [allow.publicApiKey()]);
 
 export type Schema = ClientSchema<typeof schema>;
 
 export const data = defineData({
   schema,
   authorizationModes: {
-    defaultAuthorizationMode: 'iam',
+    defaultAuthorizationMode: "apiKey",
+    apiKeyAuthorizationMode: {
+      expiresInDays: 30,
+    },
   },
 });
+
 
 /*== STEP 2 ===============================================================
 Go to your frontend source code. From your client-side code, generate a
